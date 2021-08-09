d213028abd21c873d721793bef17165464dea7e1
WissemBrinis <wissembenbrinis@gmail.com>
2021-08-09 14:27:27 +0100

    step 1

---
+++ main.js
@@ -0,0 +1,20 @@
+//  input will take a click of a button
+// we are going to chose from the three options which are : Rock, Paper, Scissor
+// for every button there is 3 probabilities  and each codition will have its own return 
+// for clicking Rock there are 3 probabilities which are : if machine randomized Rock ===> 'Tie Game'
+ //  for clicking Rock there are 3 probabilities which are : if machine randomized Scissor ===> 'You Win'
+ // for clicking Rock there are 3 probabilities which are : if machine randomized Paper ===> 'You Lose'
+ 
+ // for clicking Paper there are 3 probabilities which are : if machine randomized Paper ===> 'Tie Game'
+ //  for clicking Paper there are 3 probabilities which are : if machine randomized Scissor ===> 'You Lose'
+ // for clicking Paper there are 3 probabilities which are : if machine randomized Rock ===> 'You Win'
+ 
+ // for clicking Scissor there are 3 probabilities which are : if machine randomized Scissor ===> 'Tie Game'
+ //  for clicking Scissor there are 3 probabilities which are : if machine randomized paper ===> 'You Win'
+ // for clicking Scissor there are 3 probabilities which are : if machine randomized Rock ===> 'You Lose'
+
+
+// make counter to record number of wins, draws and loses
+
+
+// we have 3 option to randomize 
